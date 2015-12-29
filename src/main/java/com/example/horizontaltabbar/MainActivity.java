027ed3e86d408ceb4b6626b7e2cb053f153c7bc9
stack.lee <lee9310@163.com>
2015-12-29 22:51:23 +0800

    有点bug，实现对FrameLayout中的Fragment进行切换

--- src/main/java/com/example/horizontaltabbar/MainActivity.java
+++ src/main/java/com/example/horizontaltabbar/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.horizontaltabbar;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.horizontaltabbar.MyView.HorizontalTabBar;
+import com.example.horizontaltabbar.fragment.FirstFragment;
+import com.example.horizontaltabbar.fragment.SecondFragment;
+import com.example.horizontaltabbar.fragment.ThirdFragment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private HorizontalTabBar tabBar;
     private View slideLine;
+    private ArrayList<String> tabNames;
+    private FrameLayout content;
+    private ArrayList<Fragment> fragmentsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         slideLine = findViewById(R.id.slideLine);
         tabBar = (HorizontalTabBar) findViewById(R.id.horizontalview);
-        tabBar.setSlideLine(slideLine);
-        tabBar.setSlideLineWidth();
-        tabBar.setTabNum(10);
+        content = (FrameLayout) findViewById(R.id.content);
+
+        tabBar.setOnPutSlideLineListener(new HorizontalTabBar.OnPutSlideLineListener() {
+            @Override
+            public View onPutSlide() {
+                return slideLine;
+            }
+        });
+        FragmentManager manager = getSupportFragmentManager();
+
+        tabNames = new ArrayList<String>();
+        tabNames.add("红色");
+        tabNames.add("绿色");
+        tabNames.add("蓝色");
+        fragmentsList = new ArrayList<Fragment>();
+        fragmentsList.add(new FirstFragment());
+        fragmentsList.add(new SecondFragment());
+        fragmentsList.add(new ThirdFragment());
+        tabBar.setFragment(fragmentsList, R.id.content, getSupportFragmentManager());
+        tabBar.setTabName(tabNames);
     }
 }
--- src/main/java/com/example/horizontaltabbar/MyView/HorizontalTabBar.java
+++ src/main/java/com/example/horizontaltabbar/MyView/HorizontalTabBar.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -27,9 +29,9 @@ public class HorizontalTabBar extends HorizontalScrollView {
     private int lineWidth;
     private int padding;
     private int lineYPoint;
-    private int tabNum;
+    private int tabNum = 0;
 
-    private int tabId = 0;
+    private int tabId = 0x0;
 
     private RadioGroup tabGroup;
     private RadioButton itmeSelect;
@@ -60,19 +62,26 @@ public class HorizontalTabBar extends HorizontalScrollView {
         mPaint.setStrokeWidth(5.0F);
     }
 
-    public void setTabNum(int num) {
-        this.tabNum = num;
-    }
 
+    private ArrayList<String> tabNames = null;
+
+    /*set tab name ,this is must be setting if you not to set ,the system should be init a four size list*/
     public void setTabName(ArrayList<String> nameList) {
         tabNames = nameList;
     }
 
-    private ArrayList<String> tabNames = null;
+    public void setTabName(String[] names) {
+        if (tabNames == null) {
+            tabNames = new ArrayList<String>();
+            for (String name : names) {
+                tabNames.add(name);
+            }
+        }
+    }
 
     private void initTabList() {
         tabNames = new ArrayList<String>();
-        for (int i = 0; i < tabNum; i++) {
+        for (int i = 1; i <= 5; i++) {
             tabNames.add("TAB-" + i);
         }
     }
@@ -81,38 +90,30 @@ public class HorizontalTabBar extends HorizontalScrollView {
         if (tabNames == null) {
             initTabList();
         }
-        if (tabNum > 0) {
-            if (tabNum < (screenWidth / lineWidth)) {
-                lineWidth = screenWidth / tabNum;
+        tabNum = tabNames.size();
+        if (tabNum < (screenWidth / lineWidth)) {
+            lineWidth = screenWidth / tabNum;
 
-                for (int i = 0; i < tabNum; i++) {
-                    RadioButton rbt = getFixRadioButton(lineWidth);
-                    if (tabNum <= tabNames.size()) {
-                        rbt.setText(tabNames.get(i));
-                    }
-                    rbt.setId(tabId);
-                    tabId++;
-
-                    if (i == 0) {
-                        rbt.setChecked(true);
-                    }
-
-                    tabGroup.addView(rbt);
+            for (int i = 0; i < tabNum; i++) {
+                RadioButton rbt = getFixRadioButton(lineWidth);
+                rbt.setText(tabNames.get(i));
+                rbt.setId(tabId);
+                tabId++;
+                if (i == 0) {
+                    rbt.setChecked(true);
                 }
-            } else {
-                for (int i = 0; i < tabNum; i++) {
-                    RadioButton rbt = (RadioButton) LayoutInflater.from(mContext).inflate(R.layout.base_rbt, null);
-                    if (tabNum <= tabNames.size()) {
-                        rbt.setText(tabNames.get(i));
-                    }
-
-                    rbt.setId(tabId);
-                    tabId++;
-                    tabGroup.addView(rbt);
-                }
+                tabGroup.addView(rbt);
             }
-            tabGroup.check(0);
+        } else {
+            for (int i = 0; i < tabNum; i++) {
+                RadioButton rbt = (RadioButton) LayoutInflater.from(mContext).inflate(R.layout.base_rbt, null);
+                rbt.setText(tabNames.get(i));
+                rbt.setId(tabId);
+                tabId++;
+                tabGroup.addView(rbt);
+            }
         }
+        tabGroup.check(0x0);
     }
 
     private RadioButton getFixRadioButton(int width) {
@@ -126,48 +127,86 @@ public class HorizontalTabBar extends HorizontalScrollView {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 group.check(checkedId);
-                int XPoint = checkedId * (lineWidth + padding) + padding;
+                int num = Integer.valueOf(checkedId + "", 10);
+
+                Log.i("TAG", "num-->" + num);
+                int XPoint = num * (lineWidth + padding) + padding;
                 smoothScrollTo(XPoint, 0);
                 slideLineAnimation(XPoint);
+
+                if (listener != null) {
+                    listener.OnTabClick(group, checkedId, tabNum);
+                } else {
+                    // Log.i("TAG","fragments--->"+fragments+"    containerViewId--->"+containerViewId+"     manager--->"+manager);
+                    if (fragments != null && containerViewId != 0 && manager != null) {
+                        for (int i = 0; i < tabNum; i++) {
+                            if (num == i) {
+                                manager.beginTransaction().replace(containerViewId, fragments.get(i)).commit();
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         });
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    private boolean isFirst = true; //为了  自定义View  会不断的重走  onMeasure 和 onLayout
+
+    private void run() {
         tabGroup = (RadioGroup) getChildAt(0);
         itmeSelect = (RadioButton) LayoutInflater.from(mContext).inflate(R.layout.base_rbt, null);
         itmeSelect.measure(0, 0);
         padding = itmeSelect.getPaddingRight();
         lineWidth = itmeSelect.getMeasuredWidth();
-        lineYPoint = itmeSelect.getMeasuredHeight();
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed, l, t, r, b);
+        Log.i("TAG","run lineWidth-->"+lineWidth);
+        slideLine = slidelineListener.onPutSlide();
+        setSlideLineWidth();
+
+        lineYPoint = itmeSelect.getMeasuredHeight();
         putTabInGroup();
         setGroupClickListener();
+        isFirst = false;
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (isFirst) {
+            run();
+        }
     }
 
 
     private TranslateAnimation translateAnimation;
     int lastXPoint;
-
     private View slideLine;
-    public void setSlideLine(View view){
-        this.slideLine = view;
-    }
 
-    public void setSlideLineWidth(){
-        if(slideLine!=null&&lineWidth!=0){
+    public void setSlideLineWidth() {
+        if (slideLine != null && lineWidth != 0) {
+            Log.i("TAG"," setSlideLineWidth  lineWidth--->"+lineWidth);
             slideLine.setMinimumWidth(lineWidth);
         }
     }
+
+
+
+    public interface  OnPutSlideLineListener{
+        public View onPutSlide();
+    }
+    private OnPutSlideLineListener slidelineListener;
+
+    public void setOnPutSlideLineListener(OnPutSlideLineListener slidelineListener){
+        this.slidelineListener = slidelineListener;
+    }
+
+
+
     public void slideLineAnimation(int XPoint) {
-        if(slideLine!=null){
-            translateAnimation = new TranslateAnimation(lastXPoint, XPoint,0,0);
+        if (slideLine != null) {
+            translateAnimation = new TranslateAnimation(lastXPoint, XPoint, 0, 0);
             translateAnimation.setFillAfter(true);
             translateAnimation.setDuration(300);
             lastXPoint = XPoint;
@@ -176,19 +215,24 @@ public class HorizontalTabBar extends HorizontalScrollView {
     }
 
     private ArrayList<Fragment> fragments;
-    public void setFragmentList(List<Fragment> fragmentList) {
+    private FragmentManager manager;
+    private int containerViewId;
+
+    public void setFragment(List<Fragment> fragmentList, int containerViewId, FragmentManager manager) {
         this.fragments = (ArrayList<Fragment>) fragmentList;
+        this.containerViewId = containerViewId;
+        this.manager = manager;
+        manager.beginTransaction().add(containerViewId, fragmentList.get(0)).commit();
     }
 
-
-    public void useWithFrameLayout() {
-
-
+    public interface OnTabClickListener {
+        public void OnTabClick(RadioGroup group, int checkedId, int totalNums);
     }
 
-    public void useWithViewPager() {
+    private OnTabClickListener listener;
 
-
+    public void setOnTabClickListener(OnTabClickListener listener) {
+        this.listener = listener;
     }
 
 }
